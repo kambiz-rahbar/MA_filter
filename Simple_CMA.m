10cddd267cdf56f63c14562cae375479a3d42817
ANCHA <45118134+kambiz-rahbar@users.noreply.github.com>
2019-10-07 11:32:38 +0330

    Add files via upload

--- Simple_CMA.m
+++ Simple_CMA.m
@@ -8,26 +8,29 @@ y = sin(x);
 my_noise = 0.1*randn(size(y));
 z = y+my_noise;
 
-% a1 = 0.25;
-% a2 = 0.25;
-% a3 = 0.25;
-% a4 = 0.25;
-% 
-% for n = 1+3:size(x,2)
-%     y_denoise(n) = a1*z(n) + a2*z(n-1) + a3*z(n-2) + a4*z(n-3);
-% end
-
 k = 30;
 a = ones(1,k)/k;
+
 y_denoise = zeros(size(z));
 for n = k:size(x,2)
     y_denoise(n) = sum(a .* z(n-k+1:n));
+end
+
+y_centdenoise = zeros(size(z));
+for n = floor(k/2):size(x,2)-floor(k/2)
+    y_centdenoise(n) = sum(a .* z(n-floor(k/2)+1:n+floor(k/2)));
 end
 
 figure(1);
 subplot(2,2,1); plot(x,y); title('data');
 subplot(2,2,2); plot(x,my_noise); title('noise');
 subplot(2,2,3); plot(x,z); title('noisy data');
-subplot(2,2,4); plot(x,y_denoise); title('denoise data');
+
+subplot(2,2,4);  hold on;
+plot(x,y_denoise,'b');
+plot(x,y_centdenoise,'r'); 
+plot(x,y,'k');
+hold off;
+legend('MA','CMA','ref');
 
 toc
